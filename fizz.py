92de6a86e44a685c5844a32136e479418c5bedc5
ryansereno <ryansereno@protonmail.com>
2021-09-21 18:30:37 -0700

    shortened function

--- fizz.py
+++ fizz.py
@@ -1,14 +1,11 @@
 for i in range(1,100):
     if i % 3 == 0:
         if i % 5 == 0:
-            print("FixxBuzz")
+            print("FizzBuzz")
         else:
             print("Fizz")
     elif i % 5 == 0:
-        if i % 3 == 0:
-            print("FizzBuzz")
-        else:
-            print("Buzz")
+        print("Buzz")
     elif i % 3 == 0 and i % 5 ==0:
         print("FizzBuzz")
     else:
